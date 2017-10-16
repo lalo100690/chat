9ac4700e2a0e7431f0037a377ce519defb496e8c
lalo100690 <lalo_100690@hotmail.com>
2017-10-16 17:01:05 -0500

    CREACION DE LA APP
    asdf

---
+++ fun.js
@@ -0,0 +1,68 @@
+
+function inicializarChats(){
+	// if (localStorage.getItem('pagina')) {
+
+
+	// }else{
+		chats = document.getElementsByClassName('chat');
+		i=0;
+		cargarChats();
+	// }
+	
+}
+
+function cargarChats(){
+	if (i<chats.length){
+		setTimeout(function(){
+			chats[i].classList.remove('oculto');
+			i++;
+			cargarChats();
+		},100)	
+	}
+}
+
+function navegar(pagina){
+	if (pagina == "chats") {
+		document.getElementById('chats').classList.add('activo')
+		document.getElementById('grupos').classList.remove('activo')
+	}else {
+		document.getElementById('chats').classList.remove('activo')
+		document.getElementById('grupos').classList.add('activo')
+	}
+	document.querySelector('section').classList.add('salida');
+	setTimeout(function(){
+		window.location.assign(pagina + '.html');
+	},600)
+}
+
+function verChat(id, pagina){
+	localStorage.setItem('pagina',pagina);
+	document.querySelector('.circulo').classList.add('grow-circulo')
+	setTimeout(function(){
+		window.location.assign('chat.html');
+	},600)
+}
+
+function cargarChat(){
+	setTimeout(function(){
+		document.querySelector('.circulo').classList.remove('grow-circulo')
+	},300)
+}
+
+function regresoDeChat(){
+	document.querySelector('.circulo').classList.add('grow-circulo');
+	document.querySelector('.circulo').style.background = 'white';
+	pag = localStorage.getItem('pagina');
+	setTimeout(function(){
+		window.location.assign(pag+'.html');
+	},600);
+}
+
+
+
+
+
+
+
+
+
